1a1b57adaeb97b8121cc9eb6323dd2f75cf89306
Miquido Ops <mqtc@miquido.com>
2022-05-04 16:56:24 +0000

    chore(deps): bump ecs_alb_service_task from 0.57.0 to 0.64.0
    Bumps [ecs_alb_service_task](https://github.com/cloudposse/terraform-aws-ecs-alb-service-task) from 0.57.0 to 0.64.0.
    - [Release notes](https://github.com/cloudposse/terraform-aws-ecs-alb-service-task/releases)
    - [Commits](https://github.com/cloudposse/terraform-aws-ecs-alb-service-task/compare/0.57.0...0.64.0)

--- ecs-bastion.tf
+++ ecs-bastion.tf
@@ -96,7 +96,7 @@ resource "aws_security_group" "ssh" {
 }
 
 module "ecs_alb_service_task" {
-  source                         = "git::https://github.com/cloudposse/terraform-aws-ecs-alb-service-task.git?ref=0.57.0"
+  source                         = "git::https://github.com/cloudposse/terraform-aws-ecs-alb-service-task.git?ref=0.64.0"
   namespace                      = var.project
   stage                          = var.environment
   name                           = "bastion"
