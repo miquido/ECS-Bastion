32c17ed88a532b93500574c56d93669dd7602019
Marek Mościchowski <marek.moscichowski@miquido.com>
2023-06-27 12:42:49 +0200

    fix: fmt

--- ecs-bastion.tf
+++ ecs-bastion.tf
@@ -84,9 +84,9 @@ module "bastion_pubkeys" {
 }
 
 resource "aws_s3_object" "object" {
-  bucket  = module.bastion_pubkeys.bucket_id
-  key     = "keys.txt"
-  content = var.public_ssh_keys
+  bucket                 = module.bastion_pubkeys.bucket_id
+  key                    = "keys.txt"
+  content                = var.public_ssh_keys
   server_side_encryption = "AES256"
 }
 
